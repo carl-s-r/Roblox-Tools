d93eb892a7fc7f75829aa0faed6f6f0f0c7677b7
carl-s-r <117726359+carl-s-r@users.noreply.github.com>
2022-11-08 22:58:57 +0800

    Create autofarm.lua

---
+++ Bedwars/autofarm.lua
@@ -0,0 +1,18 @@
+repeat
+	wait()
+until game:IsLoaded()
+
+syn.queue_on_teleport("loadstring(game:HttpGet(''))()")
+
+while (wait(10)) do
+	local redTeam = game:GetService("Teams")["Red"]:GetPlayers()
+	local blueTeam = game:GetService("Teams")["Blue"]:GetPlayers()
+	if #redTeam == 0 or #blueTeam == 0 then
+		local A_1 = 
+			{
+				["queueType"] = "bedwars_20v20"
+			}
+		local Event = game:GetService("ReplicatedStorage")["events-@easy-games/lobby:shared/event/lobby-events@getEvents.Events"].joinQueue
+		Event:FireServer(A_1)
+	end
+end
